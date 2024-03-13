be4c9fd9c528eef8ae018b2f16b5f7fc26aef236
Joabe17 <jbee.silva17@gmail.com>
2024-03-13 13:11:04 -0300

    primeiro commit

---
+++ atividadeSom1.java
@@ -0,0 +1,16 @@
+package atividadesSom;
+
+public class atividadeSom1 {
+
+	public static void main(String[] args) {
+		String espacos = "Tedro Tescava Teixes no Tier";
+
+		String corrigir = espacos.replace("T", "P");
+
+		System.out.println("Antes: "+ espacos +" Depois: " + corrigir);
+
+
+
+	}
+
+}
---
+++ atividadesom2.java
@@ -0,0 +1,10 @@
+package atividadesSom;
+
+public class atividadesom2 {
+	public static void main(String[] args) {
+		String frase = "Pedro Pescava Peixes no Pier ";
+		String antes = frase.substring (14, 19);
+		
+		System.out.println ("Palavra:" + antes);
+}
+}
